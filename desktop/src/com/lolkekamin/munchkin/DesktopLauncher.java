66ebb09917e38a14c6743bd4cdb486d85cee8dbf
lolkekamin <forlessons.12@gmail.com>
2022-07-28 18:11:56 +0600

    initial commit

---
+++ desktop/src/com/lolkekamin/munchkin/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.lolkekamin.munchkin;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.lolkekamin.munchkin.Munchkin;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Munchkin");
+		new Lwjgl3Application(new Munchkin(), config);
+	}
+}
